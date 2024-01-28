097473d9f7446cb79fb0d2840285c1cba1c0d73d
penguinPark <park.ashl@northeastern.edu>
2024-01-28 02:21:31 +0000

    Updated-FINISHED original project

--- Project1-1/MainWindow.xaml.cs
+++ Project1-1/MainWindow.xaml.cs
@@ -26,54 +26,67 @@ namespace Project1_1
             InitializeComponent();
 
         }
-        private void heightBox_TextChanged(object sender, TextChangedEventArgs e)
+
+        /* Created two instance variables; Resource: https://www.geeksforgeeks.org/c-sharp-types-of-variables/ */
+        int rectHeight; // Created an instance variable 'rectHeight' to use in multiple methods
+        int rectArea; // Created an instance variable 'rectArea' to use in multiple methods
+
+        /* Method heightBox_TextChanged will make sure that the inputted height is an integer and it's length is greater than 0 */
+        private void HeightBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // int area = 200;
-            double area = Rectangle.Height * Rectangle.Width;
-            if (heightBox.Text.Length == 0)
+            if (HeightBox.Text.Length == 0) // This to make sure that there won't be any errors when the textbox is empty
             {
                 return;
             }
-            int height = int.Parse(heightBox.Text);
-            
-            if (height !=0 && height <= 450)
+
+            int heightCheck = int.Parse(HeightBox.Text); // This is to make sure that the height/text inputted will be an integer
+            if(heightCheck > 0) // If the inputted height is greater than 0, variable rectHeight will hold the inputted height
             {
-                double width = area / height;
-                if (width <= 800 && width > 0)
-                {
-                    Rectangle.Height = height;
-                    Rectangle.Width = width;
-                }
-            } 
+                rectHeight = heightCheck;
+            }
+        }
+       /* Method heightBox_PreviewTextInput will make sure that the inputted height will take only numbers */
+        private void HeightBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result); // Resource: TextBook page 9
+        }
+
+        /* Method areaBox_TextChanged will make sure that the inputted area is an integer and it's length is greater than 0 */
+        private void AreaBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (AreaBox.Text.Length == 0) // This is to make sure that there won't be any errors when the textbox is empty
+            {
+                return;
+            }
+            rectArea = int.Parse(AreaBox.Text); 
+        }
+
+        /* Method areaBox_PreviewTextInput will make sure that the inputted area will take only numbers */
+        private void AreaBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        /* Method Button_Click creates the rectangle */
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rectHeight == 0) // If the user makes height 0, this message will come out
+            {
+                Text1.Text = "Please put something higher than a 0";
+                return;
+            }
+            double rectWidth = rectArea / rectHeight; // This is the formula for the rectangle width
+
+            if(rectHeight > 0 && rectHeight <=356 && rectWidth > 0 && rectWidth <=784) // This is to make sure that the rectangle is not too small and doesn't grow bigger than the main window; 356 and 784 are the largest height and width can be in the main window that pops out on my macbook
+            {
+                Text1.Text = "Congratulations, you made a rectangle!"; // If the rectangle has inputted area and heights that are within the boundaries, this text will show
+                Rectangle.Height = rectHeight; // The rectangle height will change to the inputted height
+                Rectangle.Width = rectWidth; // The rectangle width will change to the inputted width
+            }
             else
             {
-                return;
+                Text1.Text = "ERROR: Area or height may be too large"; // If the rectangle is too big for the main window, this message will show
             }
-        }
-
-        private void heightBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !int.TryParse(e.Text, out int result);
-        }
-
-        private void areaBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (areaBox.Text.Length == 0)
-            {
-                return;
-            }
-            int areaText = int.Parse(areaBox.Text);
-            double width = areaText / Rectangle.Height;
-            if(width <= 800 && width > 0)
-            {
-                Rectangle.Width = width;
-
-            }
-
-        }
-        private void areaBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !int.TryParse(e.Text, out int result);
         }
     }
 }
